58439dffd0ce76ef1c331f04679e31faa1b6617d
Micah Boyd <micahhope@yahoo.com>
2017-02-02 15:07:12 +1100

    carousel completed. Readme file populated

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,32 @@
-console.log('ready');
+
+// array of image URLs where images are cointained
+const images = [
+  'Assets/900x600.png', 'Assets/900x600aqua.png', 'Assets/900x600blue.png', 'Assets/900x600purple.png', 'Assets/900x600red.png'
+];
+
+// number that will change in order to change the current image
+var imageNumber = 0
+
+// variable that will change when scrolling back and forth in the carousel
+var currentImage = images[0];
+
+// clicking left the current image will change by moving one left in the images array
+$('.left-arrow').click(function() {
+imageNumber -= 1
+if (imageNumber < 0) {
+  imageNumber = images.length-1
+}
+// change the carousel background to the currentImage
+currentImage = images[imageNumber]
+$('.carousel').css("background-image", `url(${currentImage})`);
+});
+
+// clicking right the current image will change by moving one right in the images array
+$('.right-arrow').click(function() {
+  imageNumber += 1
+  if (imageNumber > images.length-1) {
+    imageNumber = 0
+  }
+  currentImage = images[imageNumber]
+  $('.carousel').css("background-image", `url(${currentImage})`);
+});
---
+++ README.md
@@ -0,0 +1,16 @@
+
+Created using
+
+Atom text editor, Chrome Dev Tools
+
+How to launch
+
+To view, open the index.html file directly in a browser.
+
+Approach taken
+
+ I chose to separate all the files involved into separate folders. This is to ensure things kept organized and simple for someone to take over. Also with the CSS, since it tends to get very lengthy, I divided each section styling into its own CSS file that was later imported into the master.
+
+My approach began by getting down a basic functional layout. I first started to replicate the design issued in the large format. This involved translating a rough feel of the original design without spending too much time on getting everything totally precise. For time sake I only chose to do the first half of the website.
+
+My second stage was to make what I had responsive with media queries with CSS. At this point I realized however that I did not have enough time to implement this. I wanted to showcase some of my ability with JavaScript and jQuery so I chose to use the remainder of my time to work on the carousel.
