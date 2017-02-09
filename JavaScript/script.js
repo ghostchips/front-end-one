a4f93c296a4405e496c03e2c22469d04d6bd41b4
Micah Boyd <micahhope@yahoo.com>
2017-02-09 12:43:57 +1100

    navigation points fully funtional, modal displaying correct text

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,8 +9,8 @@ const images = [
 // number that will change in order to change the current image
 var imageNumber = 0
 
-// make first nav point active
-$('#first').css('color', 'rgb(97,97,97)');
+// variable that will change when scrolling back and forth in the carousel
+var currentImage = images[0];
 
 // function that checks what number image is active and changes navigation points accordingly
 function changeNav() {
@@ -29,49 +29,82 @@ function changeNav() {
   }
 }
 
-// variable that will change when scrolling back and forth in the carousel
-var currentImage = images[0];
+// function that checks what nav point was clicked and change the carousel image accordingly
+function clickNav(clicked) {
+  if (clicked === 'first') {
+    imageNumber = 0
+  } else if (clicked === 'second') {
+    imageNumber = 1
+  } else if (clicked === 'third') {
+    imageNumber = 2
+  }
+  currentImage = images[imageNumber];
+  $('.carousel').css("background-image", `url(${currentImage})`);
+}
 
-// preload each image file, giving 20 milliseconds for each to load
-images.forEach(image => {
-  setTimeout(function() {
-    $('.carousel-loader').css("background-image", `url(${image})`);
-  }, 200);
-})
-
-// clicking left the current image will change by moving one left in the images array
-$('.left-arrow').click(function() {
-  imageNumber -= 1
+function changeCarousel(clicked) {
+  // check wich direction to move in the images array
+  if (clicked === 'left') {
+    imageNumber -= 1
+  } else if (clicked === 'right') {
+    imageNumber += 1
+  }
+  // check to see if image number has gone outside the array of images
   if (imageNumber < 0) {
     imageNumber = images.length-1
+  } else if (imageNumber > images.length-1) {
+    imageNumber = 0
   }
-  changeNav();
-  // change the carousel background to the currentImage
+  // change carousel background to the current image
   currentImage = images[imageNumber]
   $('.carousel').css("background-image", `url(${currentImage})`);
+};
+
+// clicking left the current image will change by moving one left in the images array
+$('.left-arrow').click(function(event) {
+  changeCarousel(event.target.id);
+  changeNav();
 });
 
 // clicking right the current image will change by moving one right in the images array
 $('.right-arrow').click(function() {
-  imageNumber += 1
-  if (imageNumber > images.length-1) {
-    imageNumber = 0
-  }
+  changeCarousel(event.target.id);
   changeNav();
-  currentImage = images[imageNumber]
-  $('.carousel').css("background-image", `url(${currentImage})`);
 });
 
+// Clicking nav-points cause carousel to change
+$('#first').click(function(event) {
+  clickNav(event.target.id);
+  changeNav();
+});
+$('#second').click(function(event) {
+  clickNav(event.target.id);
+  changeNav();
+});
+$('#third').click(function(event) {
+  clickNav(event.target.id);
+  changeNav();
+});
+
+// carousel slideshow loop
+setInterval(function() {
+  changeCarousel('right')
+  changeNav();
+}, 5000);
+
+// preload each image file, giving 20 milliseconds for each to load
+images.forEach(image => {
+    $('.carousel-loader').css("background-image", `url(${image})`);
+})
 
 // ----------->> CAROUSEL END
 
 // ---- MODAL LOGIC -------->>
 
 $('.image-modal').click(function(event) {
-  // get blurb text from clicked image
-  var $target = $(event.target);
+  // get blurb text from clicked image, located in div 'large-image-text' below image
+  var $target = $(event.target.lastElementChild);
   var targetBlurb = $target.text();
-  console.log(targetBlurb);
   // set blurb text onto the modal
   $('.modal-text').html(`<p>${targetBlurb}</p>`);
   // reveal modal and darkened background
